1dacb8f2404a73d7e9f8891cfe48afacb23d1d86
motbelman <motbelman@gmail.com>
2020-05-02 00:31:09 -0500

    first commit

---
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -0,0 +1,34 @@
+package com.automation.tests.day2;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) throws Exception{
+        // to start selenium script we need:
+        // setup webdriver(browser driver   ) and create webDriver object
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        // In selenium , everything starts from WebDriver interface
+        driver.get("http://google.com"); // to open a website
+
+        Thread.sleep(3000);// for demo , wait 3 seconds
+        // method that return page title
+        // you can also see it as tab name , in the browser
+        String title = driver.getTitle(); // returns <title> Some title </title> text
+        String expectedTitle = "Google";
+
+        System.out.println("Title is...."+title );
+
+        if(expectedTitle.equals(title)) {
+            System.out.println("Test Passed!");
+        }else{
+            System.out.println("Test Failed!");
+        }
+
+        driver.close();// to close the website
+        // to close everything
+    }
+}
